5bece095252d044c00eefe4c6b09a202ca21b0a0
migyanari <miteazul@hotmail.es>
2021-02-08 16:39:07 +0000

    Arreglando errores del despliegue en heroku

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -70,7 +70,7 @@ MODULES = [
     'voting',
 ]
 
-BASEURL = 'http://localhost:8000'
+BASEURL = 'https://decideyari.herokuapp.com'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -180,3 +180,6 @@ if os.path.exists("config.jsonnet"):
 
 
 INSTALLED_APPS = INSTALLED_APPS + MODULES
+
+import django_heroku
+django_heroku.settings(locals())
